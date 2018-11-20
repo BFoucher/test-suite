1b0d68f41a3616bfc0832fc020438aacb28a634f
Baptiste <baptiste@troopers.email>
2018-11-20 15:38:01 +0100

    Update to circle-ci 2

--- dependencies.sh
+++ dependencies.sh
@@ -27,7 +27,7 @@ if [ -z "${RUN_NIGHTLY_BUILD}" ]; then
 fi
 npm install less@2.7.2 && \
 mkdir fails && \
-php -d memory_limit=-1 /usr/local/bin/composer install --prefer-dist
+php -d memory_limit=-1 /usr/bin/composer install --prefer-dist
 sum=$(( $sum + $? ))
 
 if [[ $1 != *"victoire/victoire"* ]]; then
@@ -39,7 +39,7 @@ if [[ $1 != *"victoire/victoire"* ]]; then
     # Manage varenv in sed
     sed -i 's@"name": "victoire/victoire",@"name": "victoire/victoire","repositories": [{"type": "vcs", "url": "https://github.com/'"${CIRCLE_PR_USERNAME}"'/'"${CIRCLE_PR_REPONAME}"'"}],@' composer.json
 
-    php -d memory_limit=-1 /usr/local/bin/composer require $1:dev-$branch#$revision --prefer-dist
+    php -d memory_limit=-1 /usr/bin/composer require $1:dev-$branch#$revision --prefer-dist
     sum=$(( $sum + $? ))
 fi
 
@@ -47,7 +47,7 @@ if [ -f ../../../Tests/dependencies.sh ]; then
     bash ../../../Tests/dependencies.sh
 fi
 
-(cd Bundle/UIBundle/Resources/config/ && bower install)
+(cd Bundle/UIBundle/Resources/config/ && bower install --allow-root)
 php Tests/App/bin/console --env=ci doctrine:database:create --no-debug && \
 php Tests/App/bin/console --env=ci doctrine:schema:create --no-debug && \
 php Tests/App/bin/console --env=ci cache:clear --no-debug && \
@@ -60,12 +60,10 @@ php Tests/App/bin/console --env=ci fos:js:dump --target="Tests/App/web/js/fos_js
 php Tests/App/bin/console --env=domain fos:js:dump --target="Tests/App/web/js/fos_js_routes_domain.js" --no-debug && \
 php Tests/App/bin/console --env=ci assetic:dump --no-debug
 sum=$(( $sum + $? ))
-
-wget http://selenium-release.storage.googleapis.com/2.53/selenium-server-standalone-2.53.1.jar
-nohup java -jar selenium-server-standalone-2.53.1.jar > /dev/null 2>&1 &
-php Tests/App/bin/console --env=ci server:start 127.0.0.1:8000 -r Tests/App/app/config/router_ci.php --docroot=Tests/App/web/ --no-debug > server.log 2>&1
+wget http://selenium-release.storage.googleapis.com/2.53/selenium-server-standalone-2.53.0.jar -O vendor/selenium.jar
+nohup java -jar vendor/selenium.jar &> /dev/null &
 sum=$(( $sum + $? ))
 nohup Xvfb :99 -ac 2>/dev/null &
 sleep 3
-
+php Tests/App/bin/console --env=ci server:start 127.0.0.1:8000 -r Tests/App/app/config/router_ci.php --docroot=Tests/App/web/ --no-debug > server.log 2>&1
 exit $sum
